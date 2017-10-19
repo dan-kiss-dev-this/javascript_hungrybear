fed5243a0e3fd4e4f73b9252bf0108f2777b1d2c
Dan Kiss <flowfast47@gmail.com>
2017-10-19 16:33:58 -0700

    add specs and tests around didYouGetEaten function

--- js/hungrybear.js
+++ js/hungrybear.js
@@ -10,6 +10,18 @@ export class HungryBear {
     }, 1000);
   }
 
+  didYouGetEaten() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  feed() {
+    this.foodLevel = 10;
+  }
+
 
 }
 
--- spec/hungrybear-spec.js
+++ spec/hungrybear-spec.js
@@ -22,6 +22,22 @@ describe('HungryBear', function() {
     expect(fuzzy.foodLevel).toEqual(7);
   });
 
+  it('should get very hungry if the food level drops below zero', function() {
+      fuzzy.foodLevel = 0;
+      expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should get very hungry if 10 seconds pass without feeding', function () {
+    jasmine.clock().tick(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should have a food level of ten if it is fed', function() {
+    jasmine.clock().tick(9001);
+    fuzzy.feed();
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
 
 });
 
